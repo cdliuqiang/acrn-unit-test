d329e1ce71ecf31f5786475b27ee121fca43836c
Andy Liu <andyx.liu@intel.com>
2019-06-06 17:03:41 +0800

    wp wt add read
    read-->write-->disorder three time

--- guest/x86/mem_cache.c
+++ guest/x86/mem_cache.c
@@ -580,6 +580,7 @@ void test_cache_type_wt_wp()
 
 	debug_print("wt read cache_test_size %lx\n",cache_size);
 	asm volatile ("   wbinvd\n" : : : "memory");
+	mem_cache_test_read(cache_size);
 	mem_cache_test_write(cache_size);
 	
 	disorder_access_size(cache_size);
@@ -591,6 +592,7 @@ void test_cache_type_wt_wp()
 	mem_cache_test_set_type(PT_MEMORY_TYPE_MASK1);
 	debug_print("wp read cache_test_size %lx\n",cache_size);
 	asm volatile ("   wbinvd\n" : : : "memory");
+	mem_cache_test_read(cache_size);
 	mem_cache_test_write(cache_size);
 	
 	disorder_access_size(cache_size);
