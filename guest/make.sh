2e70b470be573cdb27267856179cfc35ad59dae5
andy <andyx.liu@intel.com>
2019-06-28 13:14:43 -0400

    add make.sh native_make.sh

---
+++ guest/make.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+make clean
+./configure
+echo "make x86/"$1".raw"
+make x86/$1.raw
+echo "scp x86/"$1".raw root@10.240.178.151:/boot/acrn-unit-test"
+scp x86/$1.raw root@10.240.178.151:/boot/acrn-unit-test
+
---
+++ guest/native_make.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+make clean
+./configure
+echo "make x86/"$1".raw"
+make x86/$1.raw
+echo "scp x86/"$1".elf root@10.240.178.146:/boot/acrn-unit-test"
+scp x86/$1.elf root@10.240.178.152:/boot/acrn-unit-test
+
